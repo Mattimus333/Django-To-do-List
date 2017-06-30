560a173dea075300b960edd354964416e74a8c28
Matt <mmuhr88@gmail.com>
2017-06-30 14:28:20 -0700

    refactored FT and made a function that checks for items in the list

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # Matt wants to check out this cool app, he types the address into his browser to check it out!
@@ -35,23 +40,17 @@ class NewVisitorTest(unittest.TestCase):
         # When he hits enter the page updates and he is able to see the GO TO THE GYM as an item on the list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Go to the gym', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Go to the gym')
 
         # There is still a text box visible so he enters DO THREE SETS OF WEIGHTED SQUATS into the box.
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Do three sets of squats')
-
-        # The page updates again and now shows both items on the list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Go to the gym', [row.text for row in rows])
-        self.assertIn('2: Do three sets of squats', [row.text for row in rows])
+        # The page updates again and now shows both items on the list
+        self.check_for_row_in_list_table('1: Go to the gym')
+        self.check_for_row_in_list_table('2: Do three sets of squats')
 
         # Matt wonders if the site will remember the list, he notices there is a unique URL for him, there is some text that explains that.
         self.fail('Finish the test!')
