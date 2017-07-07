ea0b7b08634f8897b3950c96de1c45fe75e17627
Matt <mmuhr88@gmail.com>
2017-07-07 09:18:18 -0700

    added a new view, template, and url used to view a list

--- lists/tests.py
+++ lists/tests.py
@@ -37,17 +37,23 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-ever')
+        self.assertEqual(response['location'], '/lists/the-only-list-ever/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='Item1')
-        Item.objects.create(text='Item2')
 
-        response = self.client.get('/')
+class ListViewTest(TestCase):
 
-        self.assertIn('Item1', response.content.decode())
-        self.assertIn('Item2', response.content.decode())
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-ever/')
+        self.assertTemplateUsed(response, 'list.html')   
+    def test_displays_all_items(self):
+        Item.objects.create(text='item1')
+        Item.objects.create(text='item2')
+
+        response = self.client.get('/lists/the-only-list-ever/')
+
+        self.assertContains(response, 'item1')
+        self.assertContains(response, 'item2')
