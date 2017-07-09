9fb4a6e1c0f8f840421b3aa94b376b8b808b0c6b
Matt <mmuhr88@gmail.com>
2017-07-09 11:06:01 -0700

    first step of functional tests for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -27,6 +27,30 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
+    def test_layout_and_styling(self):
+        # Matt goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # He notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=10
+        )
+
+        # He starts a new list and notices the input box is nicely centered there too.
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
     def test_can_start_a_list_for_one_user(self):
 
         # Matt wants to check out this cool app, he types the address into his browser to check it out!
