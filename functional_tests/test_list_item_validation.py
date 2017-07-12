44e9fdab56be334e19fe8a3ebd8ecb503bc7b39a
Matt <mmuhr88@gmail.com>
2017-07-12 15:01:52 -0700

    implement duplicate item validation at model layer

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -31,15 +31,32 @@ class ItemValidationTest(FunctionalTest):
 
         # Again, the browser will not comply
         self.wait_for_row_in_list_table('1: Buy milk')
-        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+        self.wait_for(lambda: self.browser.find_element_by_css_selector(
             '#id_text:invalid'
         ))
 
         # And she can correct it by filling some text in
         self.get_item_input_box().send_keys('Make tea')
-        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+        self.wait_for(lambda: self.browser.find_element_by_css_selector(
             '#id_text:valid'
         ))
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
+
+    def test_cannot_add_duplicate_list_items(self):
+
+        # He starts by adding an item to a list
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Buy mints')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy mints')
+
+        # He accidently tries to add the same item
+        self.get_item_input_box().send_keys('Buy mints')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        # He sees a helpful error message
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text, "You've already got this on your list"
+        ))
