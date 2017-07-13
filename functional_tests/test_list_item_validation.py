e1f8b7200002838932f57eb2ae6200e31f24eba2
Matt <mmuhr88@gmail.com>
2017-07-13 14:33:58 -0700

    implemented new form for list view

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -58,5 +58,5 @@ class ItemValidationTest(FunctionalTest):
 
         # He sees a helpful error message
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text, "You've already got this on your list"
+            self.browser.find_element_by_css_selector('.has-error').text, "You've already got this in your list"
         ))
