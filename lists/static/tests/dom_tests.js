a389625dcdd44c55955d4db14c793d2977f9ca92
Matt <mmuhr88@gmail.com>
2017-07-19 10:26:17 -0700

    tests pass, test checks that click outside of textbox does not remove error

--- lists/static/tests/dom_tests.js
+++ lists/static/tests/dom_tests.js
@@ -17,7 +17,8 @@ QUnit.test("errors should be supressed on click inside the text element", functi
   assert.equal($('.has-error').is(':visible'), false);
 });
 
-// QUnit.test("errors aren't hidden if there is a click outside of the text element", function (assert) {
-//   window.Superlist.initialize();
-//   assert.equal($('.has-error').is(':visible'), true);
-// });
+QUnit.test("errors aren't hidden if there is a click outside of the text element", function (assert) {
+  window.Superlist.initialize();
+  $('div[id="qunit-fixture"]').trigger('click');
+  assert.equal($('.has-error').is(':visible'), true);
+});
