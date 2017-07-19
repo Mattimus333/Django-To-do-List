112d9cc37343fbb50f013a5fbea4c99fa7a9fc13
Matt <mmuhr88@gmail.com>
2017-07-19 09:19:00 -0700

    added test that checks if error is cleared when a click in textbox happens.  Test currently fails

--- lists/static/tests/dom_tests.js
+++ lists/static/tests/dom_tests.js
@@ -10,3 +10,14 @@ QUnit.test("errors aren't hidden if there is no keypress", function (assert) {
   window.Superlist.initialize();
   assert.equal($('.has-error').is(':visible'), true);
 });
+
+QUnit.test("errors should be supressed on click inside the text element", function (assert) {
+  window.Superlist.initialize();
+  $('input[name="text"]').trigger('click');
+  assert.equal($('.has-error').is(':visible'), false);
+});
+
+// QUnit.test("errors aren't hidden if there is a click outside of the text element", function (assert) {
+//   window.Superlist.initialize();
+//   assert.equal($('.has-error').is(':visible'), true);
+// });
