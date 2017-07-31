a2283e4d25b7795f091fe0e6563741106b783dab
Matt <mmuhr88@gmail.com>
2017-07-31 10:59:17 -0700

    new_list view tries to assign owner but cant

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -19,11 +19,47 @@ class MyListsTest(FunctionalTest):
             path='/',
         ))
 
-    def test_logged_in_user_lists_are_saved_as_my_lists(self):
-        email = 'matt@example.com'
-        self.browser.get(self.live_server_url)
-        self.wait_to_be_logged_out(email)
+    def test_logged_in_users_lists_are_saved_as_my_lists(self):
+        # Matt is a logged-in user
+        self.create_pre_authenticated_session('matt@example.com')
 
-        self.create_pre_authenticated_session(email)
+        # He goes to the home page and starts a list
         self.browser.get(self.live_server_url)
-        self.wait_to_be_logged_in(email)
+        self.add_list_item('Reticulate splines')
+        self.add_list_item('Immanentize eschaton')
+        first_list_url = self.browser.current_url
+
+        # He notices a "My lists" link, for the first time.
+        self.browser.find_element_by_link_text('My lists').click()
+
+        # He sees that his list is in there, named according to its
+        # first list item
+        self.wait_for(
+            lambda: self.browser.find_element_by_link_text('Reticulate splines')
+        )
+        self.browser.find_element_by_link_text('Reticulate splines').click()
+        self.wait_for(
+            lambda: self.assertEqual(self.browser.current_url, first_list_url)
+        )
+
+        # He decides to start another list, just to see
+        self.browser.get(self.live_server_url)
+        self.add_list_item('Click cows')
+        second_list_url = self.browser.current_url
+
+        # Under "my lists", his new list appears
+        self.browser.find_element_by_link_text('My lists').click()
+        self.wait_for(
+            lambda: self.browser.find_element_by_link_text('Click cows')
+        )
+        self.browser.find_element_by_link_text('Click cows').click()
+        self.wait_for(
+            lambda: self.assertEqual(self.browser.current_url, second_list_url)
+        )
+
+        # He logs out.  The "My lists" option disappears
+        self.browser.find_element_by_link_text('Log out').click()
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_elements_by_link_text('My lists'),
+            []
+        ))
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.test import TestCase
@@ -12,6 +13,8 @@ from lists.models import Item, List
 from lists.views import home_page
 
 from unittest import skip
+
+User = get_user_model()
 
 class HomePageTest(TestCase):
 
@@ -152,8 +155,22 @@ class NewListTest(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
+    def test_list_owner_is_saved_if_user_is_authenticated(self):
+        User.objects.create(email='a@b.com')
+        self.client.force_login(user)
+        self.client.post('/lists/new', data={'text': 'new item'})
+        list_ = List.objects.first()
+        self.assertEqual(list_.owner, user)
+
 class MyListsTest(TestCase):
 
     def test_my_lists_url_renders_my_lists_template(self):
+        User.objects.create(email='a@b.com')
         response = self.client.get('/lists/users/a@b.com/')
         self.assertTemplateUsed(response, 'my_lists.html')
+
+    def test_passes_correct_owner_to_template(self):
+        User.objects.create(email='wrong@owner.com')
+        correct_user = User.objects.create(email='a@b.com')
+        response = self.client.get('/lists/users/a@b.com/')
+        self.assertEqual(response.context['owner'], correct_user)
